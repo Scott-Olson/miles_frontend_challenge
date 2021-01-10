539d70b3621dade557c35328136e74d7fee4d03b
scott-olson <scotteolson3@gmail.com>
2021-01-09 20:52:06 -0800

    add directories and basic components

---
+++ src/components/Reward.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reward extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Reward;
